bad69eb64eb5e58aa5c4961242191301f7509cc8
Tobias Fink <tobifnk@gmail.com>
2025-07-28 16:06:55 +0200

    correcting the keycode for the key I cnnt type right now.

--- keyboards/crkbd/keymaps/xi/keymap.c
+++ keyboards/crkbd/keymaps/xi/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    LGUI(KC_A),    KC_S,    KC_D,    LSFT_T(KC_F),    KC_G,                         KC_H,    LSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN, XXXXXXX,
+      KC_LCTL,    LGUI_T(KC_A),    KC_S,    KC_D,    LSFT_T(KC_F),    KC_G,                         KC_H,    LSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_Z,    LCTL_T(KC_X),    LALT_T(KC_C),    KC_V,    KC_B,                         KC_N,    KC_M, LALT_T(KC_COMM),  LCTL_T(KC_DOT), KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
